40b3e3f375f23216e027b9f54c134f9887f12eaf
sdasd30 <danielzhao2@gmail.com>
2018-03-08 21:37:55 -0500

    Setup and the beggining of turret scripts

---
+++ Laser Game/Assets/Scripts/Rotate_Turret.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Turret: MonoBehaviour {
+	bool GOGOGO;
+	Rigidbody2D m_body;
+	Vector2 currentPos;
+	Select_Turret turret;
+	// Use this for initialization
+	void Start () {
+		GOGOGO = false;
+		turret = gameObject.GetComponentInParent<Select_Turret>();
+		m_body = GetComponent<Rigidbody2D>();
+		Debug.Log (currentPos);
+		currentPos = new Vector2(transform.position.x,transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (turret.aimLoc);
+		if (Input.GetKey ("a")) {
+			GOGOGO = true;
+		}
+		if (Input.GetKey ("b")) {
+			cancelTurn ();
+		}
+		if (GOGOGO) {
+			float direction = (Mathf.Atan2 (turret.aimLoc.y - currentPos.y, turret.aimLoc.x - currentPos.x)) * Mathf.Rad2Deg;
+			float angle = Mathf.MoveTowardsAngle (transform.eulerAngles.z, direction - 90, 90 * Time.deltaTime);
+			m_body.transform.localRotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
+		}
+			
+	}
+
+	void cancelTurn(){
+		GOGOGO = false;
+	}
+}
---
+++ Laser Game/Assets/Scripts/Select_Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_Turret : MonoBehaviour {
+	private bool isSelected;
+	public Vector2 aimLoc;
+	// Use this for initialization
+	void Start () {
+		aimLoc = new Vector2(0,0);
+		isSelected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (1) && isSelected == true) {
+			aimLoc = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		}
+	}
+
+	void OnMouseDown(){
+		isSelected = true;
+	}
+}
